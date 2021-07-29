afe27ba579b60e571d3ac4ee4718d432b05e1194
bosunKwak <87002218+bosunKwak@users.noreply.github.com>
2021-07-30 03:41:25 +0900

    개집사-chatting

---
+++ chatting2/chatting2/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  chatting2
+//
+//  Created by 곽보선 on 2021/07/30.
+//
+
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack{
+                AppTitle1
+            }
+        }
+        
+        ZStack{
+            VStack{
+              //  Chat()
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+private extension ContentView{
+    var AppTitle1: some View{
+        
+        VStack{
+            Text("개집사")
+                .font(.title)
+                .fontWeight(.medium)
+                .padding()
+            
+        }
+        .frame(maxHeight: 150, alignment: .center)
+    }
+}
---
+++ chatting2/chatting2/chatting2App.swift
@@ -0,0 +1,17 @@
+//
+//  chatting2App.swift
+//  chatting2
+//
+//  Created by 곽보선 on 2021/07/30.
+//
+
+import SwiftUI
+
+@main
+struct chatting2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
